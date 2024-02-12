a1356b6adaadf0adab06a8773cdf79758ab2cf0e
Mytth <Mytth75@gmail.com>
2024-02-12 16:48:00 +0700

    init

---
+++ src/js/__test__/test.js
@@ -0,0 +1,42 @@
+import Validator from '../main';
+
+const validator = new Validator('john_doe');
+
+test('Должен вернуть true если username валиден', () => {
+  expect(validator.validateUserName()).toBe(true);
+  validator.setUsername('Jho-n__12DoE');
+  expect(validator.validateUserName()).toBe(true);
+});
+
+test('Тест на латинские буквы', () => {
+  validator.setUsername('john_дoe');
+  expect(validator.validateUserName()).toBe(false);
+});
+
+test('Тест на невалидные символы', () => {
+  validator.setUsername('john_@doe');
+  expect(validator.validateUserName()).toBe(false);
+});
+
+test('Тест на символы тире и подчеркивания в нчале и конце', () => {
+  validator.setUsername('-john_doe');
+  expect(validator.validateUserName()).toBe(false);
+  validator.setUsername('john_doe-');
+  expect(validator.validateUserName()).toBe(false);
+  validator.setUsername('_john_doe');
+  expect(validator.validateUserName()).toBe(false);
+  validator.setUsername('john_doe_');
+  expect(validator.validateUserName()).toBe(false);
+});
+
+test('Тест на 3 цифры подряд', () => {
+  validator.setUsername('john_123');
+  expect(validator.validateUserName()).toBe(false);
+});
+
+test('Тест на цифры в нчале и конце имени', () => {
+  validator.setUsername('john_13');
+  expect(validator.validateUserName()).toBe(false);
+  validator.setUsername('12john_doe');
+  expect(validator.validateUserName()).toBe(false);
+});
---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+export default class Validator {
+  constructor(username) {
+    this.username = username;
+  }
+
+  validateUserName() {
+    const usernameRegex1 = /^[a-zA-Z][a-zA-Z0-9_-]*[a-zA-Z]$/;
+    const usernameRegex2 = /\d{3}/;
+    if (usernameRegex1.test(this.username) && !usernameRegex2.test(this.username)) {
+      return true;
+    }
+    return false;
+  }
+
+  setUsername(username) {
+    this.username = username;
+  }
+}
